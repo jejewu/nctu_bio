26b996a99323cdf98d4075146a6a9da95b981585
chiapresposer <chiapresposer@gmail.com>
2019-05-04 17:35:18 +0800

    haha

--- page-templates/template-staff.php
+++ page-templates/template-staff.php
@@ -6,13 +6,13 @@
 <?php get_header(); ?>
 <head>
 
-    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/intro.css" type="text/css" media="screen and (min-width: 701px)" />
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/contact.css" type="text/css" media="screen and (min-width: 701px)" />
     <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/mobile-css/intro-mobile.css" type="text/css" media="screen and (max-width: 700px)" />
 
 </head>
 
 
-<div class="page-staff">
+<!--<div class="page-staff">
     <div class="container mobile-staff">
         <div id="staff-container">
 	    <div class="staff-littleTitle formobile" style="line-height:18px;">03  院辦公室成員</div>
@@ -206,8 +206,122 @@
         </div>
     </div>
 		   <a class="icon_up"href="<?php echo site_url(); ?>/dean" style="opacity:1; transform: matrix(1,0,0,1,0,0);"><div class="left_icon"></div></a>
+</div>-->
+<div class="page-intro">
+    <div class="container intro-mobile-container">
+        <div class="intro-bigTitle_1">聯絡我們</div>
+        <!--<div class="map"><a href="https://goo.gl/maps/qo5CweUFBWG2">地理位置(On Google Map)</a></div>-->
+        <div class="ra-circle_first">
+            <div class="page-1">
+            <div class="nextpage"></div>
+            <div class="ra-circle_first_block_1">
+                <div class="ra-circle_1"></div>
+                <div class="first_block_context_block">
+                    <div class="first_first_line">行政專員</div>
+                    <div class="first_second_line">呂聖鈴</div>
+                    <div class="middle_line"><hr></div>
+                    <div class="first_third_line">研究計畫及財產等事務<br>shenq@cc.nctu.edu.tw<br>03-5712121 # 56902</div>
+                </div>
+            </div>
+            <div class="ra-circle_first_block_2">
+                <div class="ra-circle_2">
+                </div>
+                <div class="first_block_context_block">
+                    <div class="first_first_line">技士</div>
+                    <div class="first_second_line">郭珍佑</div>
+                    <div class="middle_line"><hr></div>
+                    <div class="first_third_line"><p>館舍及公儀維修 門禁管理</p><p>系教評會業務 排課</p><p>shenq@mail.nctu.edu.tw</p><p>03-5712121 # 56901</p></div>
+                </div>
+            </div>
+            </div>
+            <div class="page-2">
+            <div class="ra-circle_first_block_1">
+                <div class="ra-circle_3"></div>
+                <div class="first_block_context_block">
+                    <div class="first_first_line">行政專員</div>
+                    <div class="first_second_line">郭淑卿</div>
+                    <div class="middle_line"><hr></div>
+                    <div class="first_third_line">院務及研究生相關事務<br>sunny@mail.nctu.edu.tw<br>03-5712121 # 56937</div>
+                </div>
+            </div>
+            <div class="ra-circle_first_block_2">
+                <div class="ra-circle_7">
+                </div>
+                <div class="first_block_context_block">
+                    <div class="first_first_line">行政專員</div>
+                    <div class="first_second_line">郭貞賢</div>
+                    <div class="middle_line"><hr></div>
+                    <div class="first_third_line"><p>科技部計畫、系所會務、院士事務</p><p>g0431@nctu.edu.tw</p><p>03-5712121 # 56938</p></div>
+                </div>
+            </div>
+            </div>
+        </div>
+        <div class="ra-circle_second">
+            <div class="page-1">
+            <div class="ra-circle_second_block_1">
+                <div class="ra-circle_4">
+                </div>
+                <div class="first_block_context_block">
+                    <div class="first_first_line">行政專員</div>
+                    <div class="first_second_line">吳佳文</div>
+                    <div class="middle_line"><hr></div>
+                    <div class="first_third_line">專題演講 實驗室環安<br>chiawen@mail.nctu.edu.tw<br>03-5712121 # 56943</div>
+                </div>
+            </div>
+            <div class="ra-circle_second_block_2">
+                <div class="ra-circle_5"></div>
+                <div class="second_block_context_block">
+                    <div class="first_first_line">行政專員</div>
+                    <div class="first_second_line">陳珮筠</div>
+                    <div class="middle_line"><hr></div>
+                    <div class="first_third_line">大學部及國際處師生<br>教學空間設備相關事務<br>g0630@nctu.edu.tw<br>03-5712121 # 56930</div>
+                </div>
+            </div>
+            </div>
+            <div class="page-2">
+            <div class="prepage"></div>
+            <div class="ra-circle_second_block_1">
+                <div class="ra-circle_6"></div>
+                <div class="first_block_context_block">
+                    <div class="first_first_line">工友</div>
+                    <div class="first_second_line">葉連發</div>
+                    <div class="middle_line"><hr></div>
+                    <div class="first_third_line">館舍維護 收發信件<br>sunny@mail.nctu.edu.tw<br>03-5712121 # 53008</div>
+                </div>
+            </div>
+            <div class="ra-circle_second_block_2">
+                <div class="ra-circle_8"></div>
+                <div class="second_block_context_block">
+                    <div class="first_first_line">行政專員</div>
+                    <div class="first_second_line">賴美伶</div>
+                    <div class="middle_line"><hr></div>
+                    <div class="first_third_line">國際處事務<br>教學空間設備相關事務<br>meiling@mail.nctu.edu.tw<br>03-5712121 # 56983</div>
+                </div>
+            </div>
+            </div>
+        </div>
+                
+    </div>
+                <a class="icon_up"href="<?php echo site_url(); ?>/dean" style="opacity:1; transform: matrix(1,0,0,1,0,0);"><div class="left_icon"></div></a>
+<!--
+    <div id="icon_down_wrapper" class="icon_down_slide"></div>
+    <a href="<?php echo site_url(); ?>/dean" style="opacity:1; transform: matrix(1,0,0,1,0,0);">
+        <span onclick="iconDown()" class="icon_down"></span>
+    </a>-->
+
 </div>
 <script>
+    $(document).ready(function(){
+  $(".page-2").hide();
+  $(".nextpage").click(function(){
+    $(".page-1").hide();
+    $(".page-2").fadeIn(1000,"swing");
+  });
+  $(".prepage").click(function(){
+    $(".page-1").fadeIn(1000,"swing");
+    $(".page-2").hide();
+  });
+});
 /*
     now = 1;
     num = 6;
