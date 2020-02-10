f271e40688b1949c96137c28fb5ff79d1e2aaa1d
OhMyBuggg <sa0963278610@gmail.com>
2020-02-10 15:07:35 +0800

    change dean

--- page-templates/template-dean.php
+++ page-templates/template-dean.php
@@ -24,14 +24,14 @@ get_header();
  
 <div class="mobile-photo"></div>
 <div class="page-dean">
-    <div class="dean-photo"></div>
+    <!--<div class="dean-photo"></div>-->
     <div class="container">
         <div id="vice_dean-container" class="mobilehidden">
             <div class="intro-littleTitle2">02  院長簡介</div>
-            <div class="dean-bigTitle">陳  俊勳</div>
+            <div class="dean-bigTitle">楊  進木</div>
             <div class="dean-littleTitle">院長</div>
-            <div class="dean-littleTitle">(2019.2~)</div>
-            <a target="_blank" href="https://cbt.nctu.edu.tw/wp-content/uploads/CVCHiun-Hsun-Chen.doc"><div class="dean-cv_icon"></div></a>
+            <div class="dean-littleTitle">(2020.02~)</div>
+            <!--<a target="_blank" href="https://cbt.nctu.edu.tw/wp-content/uploads/CVCHiun-Hsun-Chen.doc"><div class="dean-cv_icon"></div></a>-->
             <div class="clear_both"></div>
             <hr class="dean_hr mobilehidden">
             <div class="dean-option-wrapper dean-height1">
@@ -58,9 +58,9 @@ get_header();
 	<div id="vice_dean-container" class="formobile">
             <div class="dean-option-wrapper dean-height3 formobile">
                 <div class="dean-littleTitle formobile">02 院長簡介</div>
-                <div class="dean-bigTitle">陳  俊勳</div>
-                <div class="dean-littleTitle">院長(2019.2-)</div>
-                <div class="notes dean-width3">電子郵件 chchen@mail.nctu.edu.tw<br>連絡電話 886-3-5131557</div>
+                <div class="dean-bigTitle">楊  進木</div>
+                <div class="dean-littleTitle">院長(2020.02~)</div>
+                <div class="notes dean-width3">電子郵件 moon@faculty.nctu.edu.tw<br>連絡電話 886-3-5712121 ext.56942</div>
             </div>
 	    <div class="dean-triangle"></div>
 	    <div class="dean-rectangle"></div>
